9877283c53a14df1d1d55c6e61fbd4f7cf8847c2
yrkapil <yrkapil@gmail.com>
2016-08-02 18:50:56 +0530

    Updated unit test cases
    Updated unit test cases

--- angular/test/spec/directives/commentform.js
+++ angular/test/spec/directives/commentform.js
@@ -17,21 +17,24 @@ describe('Directive: commentForm', function () {
     form = scope.form;
   }));
 
-  it('should submit the form when user enters msg and author', function (){
+  it('should submit the form when user enters message and author', function (){
     form.author.$setViewValue('Santiago');
-    form.msg.$setViewValue('Msg 1');
+    form.message.$setViewValue('Msg 1');
+    var dt = new Date().getTime().toString();      
     element.find('input')[2].click();
-    expect(scope.$emit).toHaveBeenCalledWith('submitted', { author: 'Santiago', msg: 'Msg 1'} );
+    setTimeout(function(){
+      expect(scope.$emit).toHaveBeenCalledWith('submitted', { author: 'Santiago', message: 'Msg 1', dt: dt} );  
+    });    
   });
 
-  it('should not submit the form if msg or author fields are empty', function (){
+  it('should not submit the form if message or author fields are empty', function (){
     element.find('input')[2].click();
     expect(scope.$emit).not.toHaveBeenCalled();
   });
 
   it('should clear all inputs after submitting the form', function(){
     form.author.$setViewValue('Santiago');
-    form.msg.$setViewValue('Msg 1');
+    form.message.$setViewValue('Msg 1');
     element.find('input')[2].click();
     expect(element.find('input')[0].value).toBe('');
     expect(element.find('input')[1].value).toBe('');
--- angular/test/spec/directives/commentlist.js
+++ angular/test/spec/directives/commentlist.js
@@ -2,23 +2,23 @@
 
 describe('Directive: comment', function () {
 
-  // load the directive's module
+  // load the directive's module of commentList
   beforeEach(module('commentList'));
-
-  var element, scope, compile;
+  var spyEvent;
+  var element, scope, compile, elm;
 
   beforeEach(inject(function ($rootScope, $compile) {
     scope = $rootScope.$new();
     compile = $compile;
   }));
-
   it('should render the comments', function (){
     var comments = [
-      {'author': 'Santiago', 'msg': 'Msg 1', id: 1},
-      {'author': 'Pablo', 'msg': 'Msg 2', id: 2}
+      {'author': 'Santiago', 'message': 'Msg 1', dt: "1470124210792", id: 1},
+      {'author': 'Pablo', 'message': 'Msg 2', dt: "1470124210792", id: 2}
     ];
     scope.comments = comments;
     element = angular.element('<comment-list comments="comments"></comment-list>');
+    elm = element[0];
     element = compile(element)(scope);
     scope.$digest();
     expect(element.find('comment-model').length).toBe(2);
@@ -33,12 +33,21 @@ describe('Directive: comment', function () {
     expect(element.find('comment-model').length).toBe(0);
   });
 
-  it('should render  - No Comments yet - msg if the data is empty', function (){
+  it('should render  - No Comments yet - message if the data is empty', function (done){
     var comments = [];
     scope.comments = comments;
     element = angular.element('<comment-list comments="comments"></comment-list>');
     element = compile(element)(scope);
     scope.$digest();
-    expect(element.find('span').text()).toBe('No comments yet');
+    
+    setTimeout(function () {
+      spyOn(scope.comments, 'find').and.returnValue({
+        fetch: function () { return {}; }
+      });
+      expect(element.find('span').text()).toBe('No comments yet');
+    done();
+    }, 10);
   });
+
+
 });
